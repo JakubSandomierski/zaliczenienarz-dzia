d021784b68f05fdcd943454246e1451d2feed6c8
JakubSandomierski <165052984+JakubSandomierski@users.noreply.github.com>
2024-05-21 17:58:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# main.py
+
+import pars
+import json_składnia
+import json_zgodność
+import yaml_weryfikacja
+import yaml_zgodność
+import xml_weryfikacja
+import xml_zgodność
+
+def main():
+    pars.some_function()
+    json_składnia.some_function()
+    json_zgodność.some_function()
+    yaml_weryfikacja.some_function()
+    yaml_zgodność.some_function()
+    xml_weryfikacja.some_function()
+    xml_zgodność.some_function()
+
+if __name__ == "__main__":
+    main()
